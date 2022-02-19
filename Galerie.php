e69fea8be331acfd6a009c4805e9e0d2102c54c3
fsimon1 <fsimon@isi.nc>
2022-02-19 13:43:18 +1100

    Change layout

--- Galerie.php
+++ Galerie.php
@@ -10,12 +10,9 @@
 			<a href="images/galerie/pirate.jpg"><img class="photogalerie" src="images/galerie/small_pirate.jpg" alt="pirate"></a>
 			<a href="images/galerie/vaiana.jpg"><img class="photogalerie" src="images/galerie/small_vaiana.jpg" alt="vaiana"></a>
 			<a href="images/moyen/avengers.jpg"><img class="photogalerie" src="images/moyen/small_avengers.jpg" alt="Chateau avengers"></a>
-			<a href="images/moyen/mickey.jpg"><img class="photogalerie" src="images/moyen/small_mickey.jpg" alt="Chateau mickey"></a>
 			<a href="images/moyen/orange.jpg"><img class="photogalerie" src="images/moyen/small_orange.jpg" alt="Chateau orange"></a>
-			<a href="images/moyen/disney.jpg"><img class="photogalerie" src="images/moyen/small_disney.jpg" alt="Chateau disney"></a>
 			<a href="images/grand/cars.jpg"><img class="photogalerie" src="images/grand/small_cars.jpg" alt="Chateau cars"></a>
 			<a href="images/grand/truck.jpg"><img class="photogalerie" src="images/grand/small_truck.jpg" alt="Chateau truck"></a>
-			<a href="images/grand/carosse.jpg"><img class="photogalerie" src="images/grand/small_carosse.jpg" alt="Chateau carosse"></a>
 		</div>
 <?php $content = ob_get_clean(); ?> 
 <?php include 'view/layout.php';?>
