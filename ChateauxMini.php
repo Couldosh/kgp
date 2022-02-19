4c581ad7db5eefdb58b2d9bcfd17659f57008646
fsimon1 <fsimon@isi.nc>
2022-02-19 12:25:08 +1100

    Add Localisation in sql request

--- ChateauxMini.php
+++ ChateauxMini.php
@@ -18,7 +18,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM Chateaux,Categories WHERE (Chateaux.idCategorie = 1)  AND Chateaux.idCategorie = Categories.idCategorie AND Chateaux.Localisation = 'SOUTH' ORDER BY Chateaux.idCategorie DESC";
+$sql = "SELECT * FROM Chateaux,Categories WHERE (Chateaux.idCategorie = 1)  AND Chateaux.idCategorie = Categories.idCategorie AND Chateaux.Localisation = 'NORTH' ORDER BY Chateaux.idCategorie DESC";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
